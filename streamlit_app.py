083d6b5018f2960331d4da8b867d217d0cbbc892
Jeffrey Lemoine <jeffmlife@gmail.com>
2024-02-04 17:16:11 +0000

    make it cooler 👀

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,17 +15,20 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+inputed_number = st.number_input(label="Modulo", min_value=0, max_value=100000, value=1)
+
 
 indices = np.linspace(0, 1, num_points)
 theta = 2 * np.pi * num_turns * indices
 radius = indices
 
-x = radius * np.cos(theta)
+x = radius * np.cos(theta % (inputed_number))
 y = radius * np.sin(theta)
 
 df = pd.DataFrame({
     "x": x,
     "y": y,
+    "z": x*y,
     "idx": indices,
     "rand": np.random.randn(num_points),
 })
@@ -35,6 +38,8 @@ st.altair_chart(alt.Chart(df, height=700, width=700)
     .encode(
         x=alt.X("x", axis=None),
         y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
+        color=alt.Color("idx", legend=None, scale=alt.Scale(scheme="magma")),
+        size=alt.Size("idx", sort="ascending",legend=None, scale=alt.Scale(range=[1, 50])),
+        opacity=alt.Opacity("idx", sort="descending", legend=None, scale=alt.Scale(range=[0.1, 1]))
+        # opacity=alt.Opacity("z", legend=None, scale=alt.Scale(range=[0.1, .9]))
     ))
